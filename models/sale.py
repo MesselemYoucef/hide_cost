2044da9d533eb6508b00584d609661bda65d6444
YoucefMesselem <messelemy@gmail.com>
2021-11-29 22:44:18 +0400

    still have problem with fixing the labels apperance

--- models/sale.py
+++ models/sale.py
@@ -1,8 +1,19 @@
 from odoo import api, fields, models
 
 
-class ProductCost(models.Model):
+class ProductCostTree(models.Model):
     _inherit = "sale.order.line"
+
+    hide_cost = fields.Boolean(string="Cost Hidden", compute="_check_user_cost_privilege", default=True)
+
+    @api.depends("name")
+    def _check_user_cost_privilege(self):
+        for record in self:
+            record.hide_cost = self.env.user.has_group("hide_cost.group_see_cost_user")
+
+
+class ProductCostForm(models.Model):
+    _inherit = "sale.order"
 
     hide_cost = fields.Boolean(string="Cost Hidden", compute="_check_user_cost_privilege", default=True)
 
